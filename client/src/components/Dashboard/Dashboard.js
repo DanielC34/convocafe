1240d378fa9f54124d3548d4403028198cbf783e
Daniel Chanda <danielchanda41@gmail.com>
2024-04-11 20:22:44 +0300

    Minor Changes

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import './Dashboard.css'
-import { isAuthenticated, signout } from '../../backend';
-import { useNavigate } from 'react-router-dom'
+// import React from 'react'
+// import './Dashboard.css'
+// import { isAuthenticated, signout } from '../../backend';
+// import { useNavigate } from 'react-router-dom'
 
-const Dashboard = () => {
+// const Dashboard = () => {
 
-  const navigate = useNavigate(); //Initialize navigation
-  const authenticatedUser = isAuthenticated(); // Check if the user is authenticated
+//   const navigate = useNavigate(); //Initialized navigation
+//   const authenticatedUser = isAuthenticated(); // Check if the user is authenticated
 
-  //Function to handle signout action
-  const onSignout = () => {
-    signout(); //Perform signout action
-    console.log("User signed out");
-    navigate("/signin");
-  };
+//   //Function to handle signout action
+//   const onSignout = () => {
+//     signout(); //Performs signout action
+//     console.log("User signed out");
+//     navigate("/signin");
+//   };
 
-  return !authenticatedUser ? (
-    <h1>Please sign in</h1>
-  ) : (
-    <div className='dashboard'>
-      <button onClick={onSignout}>Sign Out</button>
-      <h1>Hello, {authenticatedUser.user.name}</h1>
-    </div>
-  );
-};
+//   return !authenticatedUser ? (
+//     <h1>Please sign in</h1>
+//   ) : (
+//     <div className='dashboard'>
+//       <button onClick={onSignout}>Sign Out</button>
+//       <h1>Hello, {authenticatedUser.user.name}</h1>
+//     </div>
+//   );
+// };
 
-export default Dashboard;
+// export default Dashboard;
